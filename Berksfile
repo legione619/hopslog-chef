402d5886dad2bcd6a6493dadb35a14be65988bf8
giabbo79 <g.boccia@almaviva.it>
2021-11-03 10:23:55 +0100

    repo 2.4

--- Berksfile
+++ Berksfile
@@ -3,12 +3,12 @@ source 'https://supermarket.chef.io'
 
 metadata
 
-cookbook 'kagent', github: "giabbo79/kagent-chef", branch: "2.3"
-cookbook 'elastic', github: "giabbo79/elasticsearch-chef", branch: "2.3"
-cookbook 'kkafka', github: "giabbo79/kafka-cookbook", branch: "2.3"
-cookbook 'conda', github: "giabbo79/conda-chef", branch: "2.3"
-cookbook 'hops', github: "giabbo79/hops-hadoop-chef", branch: "2.3"
-cookbook 'consul', github: "giabbo79/consul-chef", branch: "2.3"
+cookbook 'kagent', github: "giabbo79/kagent-chef", branch: "2.4"
+cookbook 'elastic', github: "giabbo79/elasticsearch-chef", branch: "2.4"
+cookbook 'kkafka', github: "giabbo79/kafka-cookbook", branch: "2.4"
+cookbook 'conda', github: "giabbo79/conda-chef", branch: "2.4"
+cookbook 'hops', github: "giabbo79/hops-hadoop-chef", branch: "2.4"
+cookbook 'consul', github: "giabbo79/consul-chef", branch: "2.4"
 cookbook 'java', github: "giabbo79/java", branch: "v7.0.0-1"
 
 cookbook 'elasticsearch', '~> 4.0.0'
--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,9 @@ include_attribute "elasticsearch"
 include_attribute "kagent"
 
 default['hopslog']['user']                      = node['install']['user'].empty? ? node['elastic']['user'] : node['install']['user']
+default['hopslog']['user_id']                   = node['elastic']['user_id']
 default['hopslog']['group']                     = node['install']['user'].empty? ? node['elastic']['group'] : node['install']['user']
+default['hopslog']['group_id']                  = node['elastic']['group_id']
 default['hopslog']['dir']                       = node['install']['dir'].empty? ? "/srv" : node['install']['dir']
 default['hopslog']['user-home']                 = "/home/#{node['hopslog']['user']}"
 
@@ -17,6 +19,7 @@ default['logstash']['beats']['spark_port']                   = "5044"
 default['logstash']['beats']['serving_port']                 = "5046"
 default['logstash']['beats']['python_jobs_port']             = "5051"
 default['logstash']['beats']['jupyter_port']                 = "5052"
+default['logstash']['beats']['services_port']                = "5053"
 default['logstash']['http']['port']                          = "9600"
 
 default['logstash']['systemd']                  = "true"
@@ -25,6 +28,14 @@ default['logstash']['base_dir']                 = node['hopslog']['dir'] + "/log
 default['logstash']['pid_file']                 = "/tmp/logstash.pid"
 default['logstash']['bin_dir']                  = node['logstash']['base_dir'] + "/bin"
 default['logstash']['consul_dir']               = node['logstash']['bin_dir'] + "/consul"
+default['logstash']['logs_dir']                 = "#{node['logstash']['base_dir']}/log"
+default['logstash']['data_dir']                 = "#{node['logstash']['base_dir']}/data"
+
+# Data volume directories
+default['logstash']['data_volume']['root_dir']  = "#{node['data']['dir']}/logstash"
+default['logstash']['data_volume']['logs_dir']  = "#{node['logstash']['data_volume']['root_dir']}/log"
+default['logstash']['data_volume']['data_dir']  = "#{node['logstash']['data_volume']['root_dir']}/data"
+
 #
 # Kibana
 #
@@ -36,7 +47,13 @@ default['kibana']['systemd']                    = "true"
 default['kibana']['home']                       = node['hopslog']['dir'] + "/kibana-" + "#{node['kibana']['version']}-linux-x86_64"
 default['kibana']['base_dir']                   = node['hopslog']['dir'] + "/kibana"
 default['kibana']['log_dir']                    = node['kibana']['base_dir'] + "/log"
+default['kibana']['data_dir']                   = "#{node['kibana']['base_dir']}/data"
 default['kibana']['pid_file']                   = "/tmp/kibana.pid"
+
+# Data volume directories
+default['kibana']['data_volume']['root_dir']    = "#{node['data']['dir']}/kibana"
+default['kibana']['data_volume']['log_dir']     = "#{node['kibana']['data_volume']['root_dir']}/log"
+default['kibana']['data_volume']['data_dir']    = "#{node['kibana']['data_volume']['root_dir']}/data"
 
 #
 # Filebeat
@@ -45,12 +62,21 @@ default['filebeat']['version']                  = "7.2.0"
 default['filebeat']['url']                      = "#{node['download_url']}/filebeat-oss-#{node['filebeat']['version']}-linux-x86_64.tar.gz"
 default['filebeat']['home']                     = node['hopslog']['dir'] + "/filebeat-" + "#{node['filebeat']['version']}-linux-x86_64"
 default['filebeat']['base_dir']                 = node['hopslog']['dir'] + "/filebeat"
+default['filebeat']['logs_dir']                 = "#{node['filebeat']['base_dir']}/log"
+default['filebeat']['data_dir']                 = "#{node['filebeat']['base_dir']}/data"
 default['filebeat']['systemd']                  = "true"
 default['filebeat']['pid_dir']                  = "/tmp"
 default['filebeat']['port']                     = "5000"
 
-default['filebeat']['spark_read_logs']           = node['hops']['base_dir'] + "/logs/userlogs/**/"
-default['filebeat']['skip']                      = "true"
+# Data volume directories
+default['filebeat']['data_volume']['root_dir']  = "#{node['data']['dir']}/filebeat"
+default['filebeat']['data_volume']['logs_dir']  = "#{node['filebeat']['data_volume']['root_dir']}/log"
+default['filebeat']['data_volume']['data_dir']  = "#{node['filebeat']['data_volume']['data_dir']}/data"
+
+default['filebeat']['spark_read_logs']          = node['hops']['base_dir'] + "/logs/userlogs/**/"
+
+default['logstash']['service_index']            = ".services-"
+default['kibana']['service_index_pattern']      = ".services-*"
 
 default['hopslog']['private_ips']         = ['10.0.2.15']
 default['hopslog']['public_ips']          = ['10.0.2.15']
